798e8dec3f052a4c48c8cb06fe098572d60bcb91
Dawid Stefanko <stefanko.dawid@wp.pl>
2020-11-24 12:08:23 +0100

    Added mask when mobile menu is open

--- menu.js
+++ menu.js
@@ -1,7 +1,10 @@
 let button = document.querySelector(".button");
 let menu = document.querySelector(".activeMenu");
+let mask = document.querySelector(".top-left > .mask");
 
 button.addEventListener("click", () => {
   button.classList.toggle("active");
   menu.classList.toggle("active");
+  mask.classList.toggle("active");
+  
 });
